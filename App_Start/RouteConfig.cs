58c5c6e5fd13f587c00774a90fb5c578e1b5ad8c
denizdag14 <dnxdag@gmail.com>
2024-03-27 15:45:21 +0300

    Ana Sayfaya satış ve alış piechartları eklendi.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace POStock
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Urun", action = "UrunIndex", id = UrlParameter.Optional }
+                defaults: new { controller = "Main", action = "MainIndex", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using POStock.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace POStock.Controllers
+{
+    public class MainController : Controller
+    {
+
+        DbStockEntities db = new DbStockEntities();
+
+        public ActionResult MainIndex()
+        {
+            return View();
+        }
+
+        public JsonResult GetSalesData()
+        {
+            var salesData = db.SATIS
+                .Where(s => s.URUN.IsActive == true)
+                .GroupBy(s => s.Urun)
+                .Select(g => new { ProductName = g.Key, TotalQuantity = g.Sum(s => s.Adet) })
+                .ToList();
+
+            return Json(salesData, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetPurchasesData()
+        {
+            var purchasesData = db.ALIS
+                 .Where(a => a.URUN.IsActive == true)
+                .GroupBy(a => a.Urun)
+                .Select(g => new { ProductName = g.Key, TotalQuantity = g.Sum(a => a.Adet) })
+                .ToList();
+
+            return Json(purchasesData, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
